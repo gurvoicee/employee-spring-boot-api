f04d90ba9452d1ec39cac02a2b94573421afb3d8
ahmetgurses <djdhdirieje@gmail.com>
2022-07-25 10:45:25 +0300

    hata duzeltildi

--- deneme38/src/main/java/com/example/demo/DemoApplication.java
+++ deneme38/src/main/java/com/example/demo/DemoApplication.java
@@ -1,43 +1,11 @@
 package com.example.demo;
 
-import org.apache.catalina.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-
-	}
-
-	public interface userService{
-		User getUserById(Long id);
-	}
-	@Service
-	public class UserServiceImpl implements userService{
-		@Override
-		public User getUserById(Long id) {
-			return null;
+		public static void main(String[] args) {
+			SpringApplication.run(DemoApplication.class, args);
 		}
-	}
-	@RestController
-	@RequestMapping("/api")
-
-	public class userController{
-		@Autowired
-		private userService userService;
-		@GetMapping("/api/{id}")
-		public User getUserById(@PathVariable Long id){
-			return userService.getUserById(id);
-		}
-	}
-
-
 }
